7aff115b41218bed50ea12982f52c3fe13f0a8f7
Aecio Levy <aeciolevy@gmail.com>
2017-01-10 00:38:49 +0000

    Initial commit with all my work so far

---
+++ if-statement.js
@@ -0,0 +1,7 @@
+var fruit = 'orange';
+var z = fruit.length;
+if (z > 5){
+	console.log('The fruit name has more than five characters.');
+} else {
+	console.log('The fruit name has five characters or less.');
+}
